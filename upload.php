610d9f9ca72e8e75d73f68325d0ea8a7d18a6b03
Buza Péter <szotyi41@yahoo.com>
2019-01-10 21:45:52 +0100

    Backend

--- upload.php
+++ upload.php
@@ -4,9 +4,9 @@
 	$uploads = 'uploads/' . $_FILES['file']['name'];
 
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploads)) {
-    	echo json_encode(["status" => "uploaded", "file" => $_FILES['file']]);
+		echo json_encode(["status" => "uploaded", "file" => $_FILES['file']]);
 	} else {
-   		echo json_encode(["status" => "error"]);
+		echo json_encode(["status" => "error"]);
 	}
 
 ?>
