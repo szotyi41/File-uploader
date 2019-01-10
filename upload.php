ee78859120df931d73b7909b013c13b8cfb0be96
Szotyi Lehet <32148855+szotyi41@users.noreply.github.com>
2019-01-10 21:46:27 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@
 	$uploads = 'uploads/' . $_FILES['file']['name'];
 
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploads)) {
-    	echo json_encode(["status" => "uploaded"]);
+    		echo json_encode(["status" => "uploaded"]);
 	} else {
    		echo json_encode(["status" => "error"]);
 	}
